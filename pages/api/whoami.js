c2e1b30b63a983a81b6e6285a9d20bb95bfcf862
robdll <dilillo.roberto@gmail.com>
2022-06-28 07:43:38 +0100

    update api

--- pages/api/whoami.js
+++ pages/api/whoami.js
@@ -1,9 +1,9 @@
 export default function handler(req, res) {
   res.setHeader("Content-Type", "application/json");
   const data = {
+    ipaddress: req.headers["x-forwarded-for"],
     language: req.headers["accept-language"],
     software: req.headers["user-agent"],
-    ipaddress: req.ip,
   };
   res.status(200).json(data);
 }
